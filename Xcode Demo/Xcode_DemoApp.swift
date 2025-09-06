909b83da53acb6c0e78b1a3483fe5fadc8200967
Arthur Gartner <Arthur.Gartner@gmail.com>
2025-09-06 17:34:41 -0400

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Arthur Gartner on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
